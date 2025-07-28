e087d3819d176c2461d7213f3e52511f20b47f5e
Sheikh Aminul Islam <94759040+sheiikhaminul@users.noreply.github.com>
2025-07-28 18:23:33 +0600

    Update qr_generator.py

--- QR_CODE_generator/qr_generator.py
+++ QR_CODE_generator/qr_generator.py
@@ -1,5 +1,5 @@
-import qrcode
-from qrcode.image.pil import PilImage
+import qrcode #for making qr
+from qrcode.image.pil import PilImage #for get output in desired format
 def user_input(filepath):
     url=input("give the text or URL you want to convert to QR : ")
     image_name=input("give the filename with which you want to save the QR : ")
@@ -20,21 +20,21 @@ def qr_generator(filepath):
     image_name=content[1].strip()#remove spaces,tabs,newline from both ends(not inside)
     back_color=content[2].strip()
     front_color=content[3].strip()
-    qr=qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=10,
-        border=4,
+    qr=qrcode.QRCode(  #qris object,QRcode is an class of qrcode python library(which we have imported)
+        version=1, #refers size of QR ranging 1-40..1 means the smallest  
+        error_correction=qrcode.constants.ERROR_CORRECT_H, #QR codes have built-in error correction.L- (7% error recovery), M- (15%), Q- (25%), H- (30%, highest error recovery means most robust).
+        box_size=10, #Each box (or pixel block) in the QR will be 10x10 pixels in the final image.
+        border=4, #Minimum border width (in boxes). So this means your QR will have a white border 4 boxes thick.
     
     )
-    qr.add_data(url)
-    qr.make(fit=True)
+    qr.add_data(url) #encode the URL to QR code
+    qr.make(fit=True) #This line generates the QR code structure (layout of squares).fit=true automatically picks the smallest qr version to fits the data 
 
-    image=qr.make_image(
-        back_color=back_color,
-        fill_color=front_color,
-        image_factory=PilImage
-        )
+    image=qr.make_image( #make_image is a method provided by the qrcode library
+        back_color=back_color, #color of the QR
+        fill_color=front_color, #color of the bg
+        image_factory=PilImage # PilImage tells the qrcode library to create the QR code as a regular image (PNG, JPG, etc.) using Pillow (PIL).
+        ) #pilimage ta use na korle qrcode output ta as a txt or csv dito oita access korte prtam na tai pilImage is used
     image.save(image_name)
     print(f"qr save as {image_name}")
     
